fdb6741892f7d0b7455a4baf57ee792be25dc41e
Atwaa <mohmedtamer9887@gmail.com>
2026-01-15 23:45:50 +0200

    jan 15

---
+++ A_Soltan_Strings.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i <= (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e6 + 5;
+
+void Sokan_El_Leil()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+
+    if (s1 < s2)
+        cout << -1;
+    else if (s1 > s2)
+        cout << 1;
+    else
+        cout << 0;
+}
+
+int main()
+{
+    ios;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
+
+/*
+
+git fetch origin master:tmp
+git rebase tmp
+git push origin HEAD:master
+git branch -D tmp
+*/
