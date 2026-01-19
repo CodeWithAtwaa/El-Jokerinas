9ceea0fba2101898c8bea4667fce9feb1b807187
Atwaa <mohmedtamer9887@gmail.com>
2026-01-19 19:11:36 +0200

    jan 19

---
+++ L_Diverse_Team.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+void Sokan_El_Leil()
+{
+    // First solve
+    // {
+    //     int n, m;
+    //     cin >> n >> m;
+    //     set<int> st; // for val
+    //     vi arr;      // for index
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         int x;
+    //         cin >> x;
+    //         if (!st.count(x))
+    //         {
+    //             st.insert(x);
+    //             arr.PB(i);
+    //         }
+    //     }
+
+    //     if (arr.size() < m)
+    //     {
+    //         no;
+    //     }
+    //     else
+    //     {
+    //         yes;
+    //         for (int i = 0; i < m; i++)
+    //         {
+    //             cout << arr[i] + 1 << " ";
+    //         }
+    //         cout << endl;
+    //     }
+    // }
+
+    // second Solve
+    int n, m;
+    cin >> n >> m;
+    vi arr(n);
+    cin >> arr;
+
+    vector<bool> see(105, false);
+    vector<int> index;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!see[arr[i]])
+        {
+            see[arr[i]] = true;
+            index.PB(i + 1);
+        }
+    }
+
+
+    if((int)index.size() < m) {
+        no;
+    }else{
+        yes;
+        for(int i = 0 ; i < m ;i++) {
+            cout << index[i] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int32_t main()
+{
+    ios;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
