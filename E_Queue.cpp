31e9c9e7b191e55866745d5877a1199b6cfe7cec
Atwaa <mohmedtamer9887@gmail.com>
2026-01-21 00:21:24 +0200

    jan 21

---
+++ E_Queue.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+void Sokan_El_Leil()
+{
+    queue<int> q;
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        string op;
+        cin >> op;
+        if (op == "push")
+        {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        else if (op == "pop")
+        {
+            if (!q.empty())
+            {
+                q.pop();
+            }
+        }
+        else if (op == "front")
+        {
+            if (!q.empty())
+            {
+                cout << q.front() << "\n";
+            }
+        }
+        else if (op == "back")
+        {
+            if (!q.empty())
+            {
+                cout << q.back() << "\n";
+            }
+        }
+    }
+}
+
+int32_t main()
+{
+    ios;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
---
+++ I_Permutation_Minimization_by_Deque.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+void Sokan_El_Leil()
+{
+    dd;
+    deque<int> dq;
+   dq.push_back(arr[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < dq.front())
+        {
+            dq.push_front(arr[i]);
+        }
+        else
+        {
+            dq.push_back(arr[i]);
+        }
+    }
+
+    for (int i = 0; i < dq.size(); i++)
+    {
+        if (i)
+            cout << " ";
+        cout << dq[i];
+    }
+    cout << endl;
+}
+
+int32_t main()
+{
+    ios;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
