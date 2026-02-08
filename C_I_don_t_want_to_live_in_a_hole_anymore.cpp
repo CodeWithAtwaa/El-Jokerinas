26cb420767f22f0270a0d21b1446de0306a06163
Atwaa <mohmedtamer9887@gmail.com>
2026-02-08 23:28:41 +0200

    feb 8

---
+++ C_I_don_t_want_to_live_in_a_hole_anymore.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+void Sokan_El_Leil()
+{
+    int n;
+    cin >> n;
+    int arr[] = {4, 7, 44, 47, 74, 77, 777, 774, 747, 744, 444, 477, 447, 474};
+
+    for(int i = 0; i < 14 ;i++ ) {
+        if(n %  arr[i] == 0) {
+            yes
+            return;
+        }
+    }
+
+    no;
+}
+
+int32_t main()
+{
+    ios;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
