6f11779e51c6748f96a8d3672cb76514209bc4e4
Atwaa <mohmedtamer9887@gmail.com>
2026-02-12 23:21:56 +0200

    feb 12

---
+++ A_Football.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+void Sokan_El_Leil()
+{
+    int n;
+    cin >> n;
+    vector<string> arr(n);
+    map<string, int> mp;
+    cin >> arr ;
+    int mx = 0;
+    for (auto it : arr)
+    {
+        mp[it]++;
+    }
+
+    string maxx = "";
+    for (auto it : mp)
+    {
+        // cout << it.first << it.second << "\n";
+        if (it.second > mx)
+        {
+            mx = it.second;
+            maxx = it.first;
+        }
+    }
+
+    cout << maxx << "\n";
+}
+
+int32_t main()
+{
+    ios;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
