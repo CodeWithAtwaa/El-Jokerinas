c2b190b3806f6c3782876dd4cdee01f5ef1be075
Atwaa <mohmedtamer9887@gmail.com>
2026-02-19 21:15:19 +0200

    feb 19

---
+++ B_The_Time.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+void Sokan_El_Leil()
+{
+    string s1;
+    int n;
+    cin >> s1 >> n;
+
+    string s2 = s1.substr(0, 2);
+    string s3 = s1.substr(3, 5);
+
+    int h = stoi(s2);
+    int m = stoi(s3);
+    int total = h * 60 + m + n;
+    int new_h = (total / 60) % 24;
+    int new_m = total % 60;
+    cout << (new_h < 10 ? "0" : "") << new_h << ":" << (new_m < 10 ? "0" : "") << new_m << ln;
+}
+
+int32_t main()
+{
+    ios;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
