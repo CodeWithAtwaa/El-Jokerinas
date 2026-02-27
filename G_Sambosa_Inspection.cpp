489a9cc14ec80972c2b3b2ee7e9cce710b1478fa
Atwaa <mohmedtamer9887@gmail.com>
2026-02-27 22:41:03 +0200

    feb 27

---
+++ G_Sambosa_Inspection.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+void Sokan_El_Leil()
+{
+    int x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    int ab = sqrt(pow(y2 - y1, 2) + pow(x2 - x1, 2));
+    int ac = sqrt(pow(y3 - y1, 2) + pow(x3 - x1, 2));
+    int bc = sqrt(pow(y3 - y2, 2) + pow(x3 - x2, 2));
+
+    int sab = pow(ab, 2);
+    int sac = pow(ac, 2);
+    int sbc = pow(bc, 2);
+
+    if(sab > (sac+ sbc) || sac > (sab+ sbc) || sbc > (sab+ sac ) ) {
+        cout << ""
+    }
+}
+
+int32_t main()
+{
+    ios;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
