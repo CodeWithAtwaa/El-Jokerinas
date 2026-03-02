32e2319be1718983bb2d6276a4d0d1bdd0aa4ac7
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-03-02 13:38:33 +0200

    dictionary in python

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+dic = {
+    'name':"Mohamed",
+    'age':20,
+    'collage': "FCI-ZU",
+    'year': 2027
+}
+
+print(dic['name'])
+print(dic['age'])
+print(dic['collage'])
+print(dic['year'])
+
+dic['sport'] = 'Football'
+print(dic)
+print('age' in dic)
