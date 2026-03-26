529e574280905a8155cc6819440ae7186ace92a9
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-03-26 22:50:26 +0200

    solve Anton and dank

---
+++ P_Anton_and_Danik.cpp
@@ -0,0 +1,105 @@
+/*
+___________________________________________________________________________________________________
+                             بِسْمِ اللَّـهِ الرَّحْمَـ ٰنِ الرَّحِيمِ
+___________________________________________________________________________________________________
+                                     Author Atwaa
+___________________________________________________________________________________________________
+
+Time : O( )
+Space : O( )
+___________________________________________________________________________________________________
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define str   \
+    string s; \
+    cin >> s;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ull unsigned long long
+#define ll long long
+#define int ll
+#define vi vector<ll>
+#define vvi vector<vi>
+#define pri pair<int, int>
+#define prl pair<ll, ll>
+// ==================
+// 2D array
+// vector<vector<int>> arr(rows, vector<int>(cols));
+// vvi arr(n, vi(m));
+// ==================
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+void Sokan_El_Leil()
+{
+    int n; cin >> n;
+    string s;
+    cin >> s;
+    int cnta = 0, cntd = 0;
+
+    for (auto &it : s)
+    {
+        if(it== 'A') cnta++;
+        else cntd++;
+    }
+
+    if(cnta > cntd) cout << "Anton\n";
+    else if(cntd >  cnta) cout << "Danik\n";
+    else cout <<"Friendship\n";
+}
+
+int32_t main()
+{
+    ios;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
