9f92c07e1e949624c71a521c818c55cfed147234
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-04-29 22:15:31 +0200

    And

---
+++ A_Prime_Queries.cpp
@@ -0,0 +1,106 @@
+/*
+___________________________________________________________________________________________________
+                             بِسْمِ اللَّـهِ الرَّحْمَـ ٰنِ الرَّحِيمِ
+___________________________________________________________________________________________________
+                                     Author Atwaa
+___________________________________________________________________________________________________
+
+Time : O( )
+Space : O( )
+___________________________________________________________________________________________________
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define ull unsigned long long
+#define ll long long
+#define int ll
+#define vi vector<ll>
+#define vvi vector<vi>
+// ==================
+// 2D array
+// vector<vector<int>> arr(rows, vector<int>(cols));
+// vvi arr(n, vi(m));
+// ==================
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+
+bool isPrime(int n){
+    if(n <= 1)
+        return false;
+    if(n == 2 || n == 3)
+        return true;
+    if(n % 2 == 0)
+        return false;
+    for (int i = 3; i <= sqrt(n); i+=2)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+    
+}
+inline void  Sokan_El_Leil()
+{
+    int n;
+    cin >> n ;
+
+    if(isPrime(n))
+        yes
+    else
+        no
+
+}
+
+int32_t main()
+{
+    ios;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
