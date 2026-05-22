15754b92de3c2c86d3ec0f49f5783f84925f31c3
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-05-22 22:36:05 +0200

    sieve

---
+++ A_Aliquot_Sum.cpp
@@ -0,0 +1,112 @@
+/*
+___________________________________________________________________________________________________
+                             بِسْمِ اللَّـهِ الرَّحْمَـ ٰنِ الرَّحِيمِ
+___________________________________________________________________________________________________
+                                     Author Atwaa
+___________________________________________________________________________________________________
+
+Time : O( )
+Space : O( )
+___________________________________________________________________________________________________
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// g++ -g -O0 temp.c++ -o temp
+// gdb ./temp
+// ctrl + shift + b
+// f5
+
+// ===================== MACROS ===================== //
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define all(vec) vec.begin(), vec.end()
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+
+#define ull unsigned long long
+#define ll long long
+#define int ll
+#define vi vector<ll>
+#define vvi vector<vi>
+// ==================
+// 2D array
+// vector<vector<int>> arr(rows, vector<int>(cols));
+// vvi arr(n, vi(m));
+// ==================
+
+#define ln "\n";
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+
+#define dd     \
+    int n;     \
+    cin >> n;  \
+    vi arr(n); \
+    cin >> arr;
+
+#define ios                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+const int MOD = 1e7;
+
+const int MAXN = 1000000;
+int aliquot_sum[MAXN + 1];
+
+void sieve()
+{
+    for (int i = 1; i <= MAXN / 2; i++)
+    {
+        for (int j = 2 * i; j <= MAXN; j += i)
+        {
+            aliquot_sum[j] += i;
+        }
+    }
+}
+
+inline void Sokan_El_Leil()
+{
+    int n;
+    cin >> n;
+
+    int sum = aliquot_sum[n];
+
+    if (sum == n)
+    {
+        cout << "perfect" << ln;
+    }
+    else if (sum > n)
+    {
+        cout << "abundant" << ln;
+    }
+    else
+    {
+        cout << "deficient" << ln;
+    }
+}
+
+int32_t main()
+{
+    ios;
+    sieve();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Sokan_El_Leil();
+    }
+}
